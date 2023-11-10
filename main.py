00c73b2039ceb06e9770af400aa6de1c6d527845
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-11-11 00:56:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,25 @@
 import pandas as pd
 import streamlit as st
 
-# Create a function to upload the xlsx file
-def upload_file(uploaded_file):
-    if uploaded_file is not None:
-        bytes_data = uploaded_file.read()
-        df = pd.read_excel(io.BytesIO(bytes_data), sheet_name='Fields', engine='openpyxl')
-        return df
-
-# Create a function to display the table
-def display_table(df):
-    st.table(df)
-
-# Create a function to filter the table based on user selection
-def filter_table(df):
-    options = df.columns.tolist()
-    selected_option = st.sidebar.selectbox("Filter by", options)
-    filtered_df = df[df[selected_option].notna()]
-    return filtered_df
 
 # Create the main function to run the app
-def main():
-    st.title("XLSX File Uploader and Filter")
 
-    #Uploader
-    uploaded_file = st.file_uploader("Choose your files", accept_multiple_files=True, type=['xlsx'])
-    df = upload_file(uploaded_file)
+st.title("XLSX File Uploader and Filter")
 
-    # Display the table if a file is uploaded
+#Uploader
+uploaded_file = st.file_uploader("Choose your file", accept_multiple_files=False, type=['xlsx'])
+if uploaded_file is not None:
+    bytes_data = uploaded_file.read()
+    df = pd.read_excel(io.BytesIO(bytes_data), sheet_name='Fields', engine='openpyxl')
+    #return df
     if df is not None:
-        display_table(df)
+        st.dataframe(df)
 
-        # Filter the table based on user selection
-        filtered_df = filter_table(df)
-        display_table(filtered_df)
+        options = df.columns.tolist()
+        selected_option = st.sidebar.selectbox("Filter by", options)
+        filtered_df = df[df[selected_option].notna()]
 
-if __name__ == "__main__":
-    main()
+
+
+
+st.markdown("""---""")
