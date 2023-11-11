2f6069e66695a41dfc2a83861b4e66e61fd8de09
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-11-11 21:56:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ st.title("XLSX File Uploader and Filter")
 
 #Uploader
 uploaded_file = st.file_uploader("Choose your file", accept_multiple_files=False, type=['xlsx'])
+
 if uploaded_file is not None:
     bytes_data = uploaded_file.read()
     df = pd.read_excel(io.BytesIO(bytes_data), sheet_name='Fields', engine='openpyxl')
@@ -17,14 +18,19 @@ if uploaded_file is not None:
     #if df is not None:
         #st.dataframe(df)
 
-options = df.columns.tolist()
-selected_option = st.sidebar.selectbox("Filter by", options)
+#sidebar
+if df is not None:
+    options = df.columns.tolist()
+    selected_option = st.sidebar.selectbox("Filter by", options)
+else:
+    selected_option = st.sidebar.selectbox("Filter by", "Default")
 
 st.sidebar.title("Text Input")
 user_input = st.sidebar.text_input("Enter text")
 
-if df is not None and user_input is not None:
+if selected_option != 'Default' and user_input is not None:
     filtered_df = df[df[selected_option] == user_input]
+    filtered_df = pd.DataFrame(filtered_df,columns=["FormOID", "PreText", "VariableOID", "DataFormat","IsLog","IsVisible"])
     st.dataframe(filtered_df)
 
 
