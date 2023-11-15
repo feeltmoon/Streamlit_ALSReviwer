42bed04fde41eac2c0e5ff6bea0749e173e27d6f
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-11-15 15:20:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,30 +18,8 @@ uploaded_file = st.file_uploader("Choose your file", accept_multiple_files=False
 #option_list = ['Default']
 #selected_option = st.sidebar.selectbox("Filter by", option_list)
 
-
-
 if uploaded_file is not None:
     bytes_data = uploaded_file.read()
-
-    try: 
-        #df = pd.read_excel(io.BytesIO(bytes_data), sheet_name='Fields', engine='openpyxl')
-        df = pd.read_excel(uploaded_file, sheet_name='Fields', engine='openpyxl')
-        # Header:
-        st.sidebar.title("Field Search")
-        df = df[df['DraftFieldActive'] == True]
-        option_list = ['FieldOID', 'FormOID', 'VariableOID', 'DataDictionaryName', 'PreText']
-        selected_option = st.sidebar.selectbox("Fields Search", option_list)
-        user_input = st.sidebar.text_input("Please enter a field key word")
-
-        if selected_option is not None and user_input != '':
-            with st.container():
-                filtered_df = df[df[selected_option].str.contains(user_input, case=False, na=False)]
-                filtered_df = pd.DataFrame(filtered_df,columns=["FormOID", "PreText", "FieldOID", "VariableOID", "DataFormat","DataDictionaryName", "IsLog", "IsVisible"])
-                st.markdown(''':blue[Fields] :balloon:''')
-                st.dataframe(filtered_df)
-            st.markdown("""---""")
-    except:
-        st.write("Worksheet named 'Fields' not found")
 
     # Form Search:
     try:
@@ -57,10 +35,30 @@ if uploaded_file is not None:
             filtered_df_frm = df_frm[df_frm[selected_option_frm].str.contains(user_input_frm, case=False, na=False)]
             filtered_df_frm = pd.DataFrame(filtered_df_frm,columns=["OID", "DraftFormName", "IsSignatureRequired", "LogDirection"])
             st.markdown(''':blue[Forms] :sunglasses:''')
-            st.dataframe(filtered_df_frm)
+            st.write(filtered_df_frm)
             st.markdown("""---""")
     except:
         st.write("Worksheet named 'Forms' not found")
+    
+    try: 
+        #df = pd.read_excel(io.BytesIO(bytes_data), sheet_name='Fields', engine='openpyxl')
+        df = pd.read_excel(uploaded_file, sheet_name='Fields', engine='openpyxl')
+        # Header:
+        st.sidebar.title("Field Search")
+        df = df[df['DraftFieldActive'] == True]
+        option_list = ['FieldOID', 'FormOID', 'VariableOID', 'DataDictionaryName', 'PreText']
+        selected_option = st.sidebar.selectbox("Fields Search", option_list)
+        user_input = st.sidebar.text_input("Please enter a field key word")
+
+        if selected_option is not None and user_input != '':
+            with st.container():
+                filtered_df = df[df[selected_option].str.contains(user_input, case=False, na=False)]
+                filtered_df = pd.DataFrame(filtered_df,columns=["FormOID", "PreText", "FieldOID", "VariableOID", "DataFormat","DataDictionaryName", "IsLog", "IsVisible"])
+                st.markdown(''':blue[Fields] :balloon:''')
+                st.write(filtered_df)
+            st.markdown("""---""")
+    except:
+        st.write("Worksheet named 'Fields' not found")
 
     # DataDictionary Search
     try:
@@ -75,7 +73,7 @@ if uploaded_file is not None:
             filtered_df_dictName = df_dict[df_dict[selected_option_dictName].str.contains(user_input_dictName, case=False, na=False)]
             filtered_df_dictName = pd.DataFrame(filtered_df_dictName,columns=["DataDictionaryName", "Ordinal", "UserDataString", "CodedData"])
             st.markdown(''':blue[DataDictionaryName] :tulip:''')
-            st.dataframe(filtered_df_dictName)
+            st.write(filtered_df_dictName)
             st.markdown("""---""")
     except:
         st.write("Worksheet named 'DataDictionaryName' not found")
